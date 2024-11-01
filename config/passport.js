067024efb22545581ee4371611c527981bbc7b3d
juliwhite <julietawhite30@gmail.com>
2024-11-01 16:45:06 -0600

    add prompt to passport file

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,8 @@ module.exports = function(passport){
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback'
+        callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback',
+        prompt: 'select_account'
     },
     async (accessToken, refreshToken, profile, done) => {
         // TEST
